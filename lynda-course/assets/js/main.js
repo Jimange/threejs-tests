4fe30d136fccfa96108cfe9e49e71aaa8805976f
jimange <jamesbarkerdesign@gmail.com>
2019-05-09 16:12:23 +0100

    LyndaCourse - finished first chapter

--- lynda-course/assets/js/main.js
+++ lynda-course/assets/js/main.js
@@ -1,18 +1,13 @@
-function getBox(w,h,d) 
-{
-	var geometry =	new THREE.BoxGeometry(w,h,d); 					//3 dimensions: w, h, d
-	var mat =		new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-	var mesh =		new THREE.Mesh(geometry, mat);
-
-	return mesh;
-}
-
 function init() 
 {
 	var scene = 	new THREE.Scene();
 	var box =		getBox(1,1,1);
+	var plane =		getPlane(4);
+	box.position.y = box.geometry.parameters.height / 2;				//gets box height and / 2
+	plane.rotation.x = Math.PI / 2;										//uses different unit to deg
 
 	scene.add(box);
+	scene.add(plane);
 
 	var cam =		new THREE.PerspectiveCamera(
 						45, 											//field of view
@@ -31,6 +26,26 @@ function init()
 	renderer.setSize(window.innerWidth,window.innerHeight);
 	document.getElementById('render-container').appendChild(renderer.domElement);
 	renderer.render(scene,cam);
+}
+
+function getBox(w,h,d) 
+{
+	var geometry =	new THREE.BoxGeometry(w,h,d); 						//3 dimensions: w, h, d
+	var mat =		new THREE.MeshBasicMaterial({ color: 0xffffff });	
+	var mesh =		new THREE.Mesh(geometry, mat);
+
+	return mesh;
+}
+
+function getPlane(size) {
+	var geometry =	new THREE.PlaneGeometry(size,size); 						//3 dimensions: w, h, d
+	var mat =		new THREE.MeshBasicMaterial({ 
+							color: 0xff0000,
+							side: THREE.DoubleSide 
+						});	
+	var mesh =		new THREE.Mesh(geometry, mat);
+
+	return mesh;
 }
 
 init();
